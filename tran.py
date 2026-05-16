47e40f5ebf0fb74d39f49ee9cc02fad4107742c1
hrcy39 <13617246075@163.com>
2026-05-16 16:22:44 +0800

    Sync by 13617 at 周六 2026/05/16 16:22:43.97

--- tran.py
+++ tran.py
@@ -1,4 +1,47 @@
 import math
+import cv2
+import numpy as np
+
+pi2=2*math.pi
+
+def apiece_convert_polar(img_width: int, img_height: int,img_path: str,polar_thr :str = "polar.thr"):
+    xy_txt = "save_.txt"
+    piece_convert_xy(img_path,xy_txt)
+    batch_convert_xy_to_polar(xy_txt,polar_thr,img_width,img_height)
+
+
+def piece_convert_xy(img_path: str,save_txt: str):
+
+    # 2. 读取图像+灰度化
+    img = cv2.imread(img_path)
+    h, w = img.shape[:2]
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # 3. 二值化提取线条（黑线条白背景/白线条黑背景自行调换）
+    _, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+
+    # 4. 以图像中心为原点计算坐标
+    # 图像像素原点：左上角(0,0)；中心原点：(0,0)在图片正中心
+    center_x = w / 2
+    center_y = h / 2
+
+    # 获取所有线条像素坐标
+    y_coords, x_coords = np.where(binary > 0)
+
+    points = []
+    for x, y in zip(x_coords, y_coords):
+        # 转换为【中心为原点】XY
+        cx = x - center_x
+        cy = center_y - y
+        points.append(f"{cx},{cy}")
+
+    # 5. 写入TXT
+    with open(save_txt, "w", encoding="utf-8") as f:
+        f.write("\n".join(points))
+
+    print(f"提取完成！共{len(points)}个坐标点，已保存至 {save_txt}")
+
+
 
 def deal_point(x: float, y: float, img_w: int, img_h: int, last_theta: float = 0.0) -> tuple[float, float]:
     """
@@ -13,12 +56,16 @@ def deal_point(x: float, y: float, img_w: int, img_h: int, last_theta: float = 0
     """
     # atan2=arctan函数(返回 -Pi ~ +Pi)
     arc = math.atan2(y, x)
+    last_ang = last_theta % pi2
+    if last_ang > math.pi:
+        last_ang -=pi2
+    
     # 保证角度连续
-    diff = arc - last_theta
+    diff = arc - last_ang
     if diff > math.pi:
-        diff -= 2*math.pi
+        diff -= pi2
     elif diff < -math.pi:
-        diff += 2*math.pi
+        diff += pi2
     theta = last_theta + diff
     # 勾股求(x,y)的模长
     raw_rho = math.hypot(x, y)
@@ -53,7 +100,7 @@ def batch_convert_xy_to_polar(txt_in_path: str, txt_out_path: str, img_width: in
                 px, py = map(float, line.split(","))
                 t, r = deal_point(px, py, img_width, img_height, last_angle)
                 last_angle = t
-                result_list.append(f"{t} {r}")
+                result_list.append(f"{t:.3f} {r:.3f}")
             except:
                 continue
     # 覆盖写入文件
--- ttest.py
+++ ttest.py
@@ -1,9 +1,11 @@
 import tran
-# 1. 填写图像尺寸
+
+IMG_PATH = "outputs\mao_sand.png"
 IMG_W = 1920
 IMG_H = 1080
-# 2. 输入原始坐标文件、输出极坐标文件
+
 INPUT_TXT = "xy.txt"
 OUTPUT_TXT = "polar.thr"
 # 执行批量转换
-tran.batch_convert_xy_to_polar(INPUT_TXT, OUTPUT_TXT, IMG_W, IMG_H)
+#tran.batch_convert_xy_to_polar(INPUT_TXT, OUTPUT_TXT, IMG_W, IMG_H)
+tran.apiece_convert_polar(IMG_W,IMG_H,IMG_PATH)
